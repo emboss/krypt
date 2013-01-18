f389d33741f345e6e5b457164ba2067936a3ebba
Martin Boßlet <Martin.Bosslet@gmail.com>
2013-01-18 03:19:09 +0100

    Adapt to Ruby 2.0.0 UTF-8 default encoding by explicitly specifying US-ASCII as source encoding where needed.

--- test/scratch.rb
+++ test/scratch.rb
@@ -1,32 +1,23 @@
+# encoding: US-ASCII
 require 'krypt'
 require_relative 'resources'
 require 'openssl'
 require 'stringio'
 require 'pp'
 
-class MyDigest
-  def initialize(alg)
-    @alg = alg
-  end
-
-  def digest
-    puts @alg
-  end
+def _B(bin_encode)
+  [bin_encode.reverse].pack('b*').reverse
 end
 
-class MyProvider
-  def new_service(klass, *rest)
-    if klass == Krypt::Digest
-      return MyDigest.new(rest[0])
-    end
-    nil
-  end
-end
+value = _B('010101010')
+asn1 = Krypt::ASN1::BitString.new(value)
+s = "\x03\x03\x00\x00\xAA"
 
-Krypt::Provider.register(:my, MyProvider.new)
+puts asn1.to_der == s
 
-d = Krypt::Digest.new("SHA1")
-d2 = Krypt::Digest::SHA256.new
+puts asn1.to_der.encoding
+puts s.encoding
 
-d.digest
-d2.digest
+
+
+
